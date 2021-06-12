c304ee06446a973645f6d1710a4305caffeae054
Dillon Ellis <dillonzellis@gmail.com>
2021-06-12 14:55:30 -0400

    completed game

--- section-7/Pig Game/script.js
+++ section-7/Pig Game/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
-let currentScore0 = 0;
-let currentScore1 = 0;
+let scores, currentScore, activePlayer, playing;
 
 // Selecting elements
 const score0El = document.getElementById("score--0");
@@ -10,33 +9,76 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
-const current0EL = document.getElementById("current--0");
-const current1EL = document.getElementById("current--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
 
-// Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+// functions
+function init() {
+  playing = true;
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+}
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+}
+
+init();
 
 // Events
 btnRoll.addEventListener("click", () => {
-  let diceNum = Math.trunc(Math.random() * 6 + 1);
-  diceEl.classList.remove("hidden");
-  diceEl.src = `dice-${diceNum}.png`;
+  if (playing) {
+    let dice = Math.trunc(Math.random() * 6 + 1);
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
 
-  if (diceNum !== 1 && player0El.classList.contains("player--active")) {
-    currentScore0 += diceNum;
-    current0EL.textContent = currentScore0;
-  } else if (diceNum === 1 && player0El.classList.contains("player--active")) {
-    player0El.classList.remove("player--active");
-    player1El.classList.add("player--active");
-  } else if (diceNum !== 1 && player1El.classList.contains("player--active")) {
-    currentScore1 += diceNum;
-    current1EL.textContent = currentScore1;
-  } else if (diceNum === 1 && player1El.classList.contains("player--active")) {
-    player1El.classList.remove("player--active");
-    player0El.classList.add("player--active");
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 });
+
+btnHold.addEventListener("click", () => {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 10) {
+      playing = false;
+      diceEl.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", init);
