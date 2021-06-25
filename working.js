e78c25a9009cff05b9ce85904c41b2e1f10e8848
Dillon Ellis <dillonzellis@gmail.com>
2021-06-24 22:47:01 -0400

    section coding challenge 2

--- working.js
+++ working.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const remainder = 110;
+const remainder = 114;
 const total = 314;
 
 const calcRemainder = function (current, remainder) {
-  return console.log(Math.round((current / remainder) * 100 * 100) / 100);
+  return console.log(`Remaining: ${Math.round((current / remainder) * 100 * 100) / 100} %`);
 };
 
 calcRemainder(remainder, total);
